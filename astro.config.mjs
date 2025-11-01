71f50d7df1310278e5cdef9f3325d8000fb9f520
tbodt <tblodt@icloud.com>
2025-10-31 19:56:48 -0700

    toki ante li kama lon la, nimi pi tenpo pini o awen pona

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,11 @@ export default defineConfig({
   ...deploy,
   integrations: [mdx(), sitemap()],
   redirects: {
-    "/en/": {
-      status: 301,
-      destination: "/",
-    },
+    "/en/": "/",
+    "/2025/": "/en/2025/",
+    "/2024/": "/en/2024/",
+    "/2023/": "/en/2023/",
+    "/2022/": "/en/2022/",
+    "/2021/": "/en/2021/",
   },
 });
